186e28ad7796f5178174310fd96a81e0936f2fc0
Harm <harm@bit-students.com>
2016-11-03 14:04:40 +0100

    Added main.py, and netlist

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+class Board(object):
+	"""docstring for Board"""
+	def __init__(self, x_dim, y_dim):
+		super(Board, self).__init__()
+		self.dimensions = (x_dim, y_dim)
+		self.board = [][][]
+
+	def getElementAt(self, x, y, z):
+		return self.board[x][y][z]
+
+	def setElementAt(self, x, y, z, object):
+		if getElementAt(x, y, z) == None:
+			
