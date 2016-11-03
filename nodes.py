dd547aec4ef4038f67945165e7603f096e78f9ee
Aynel Gül <Aynel@wcw-stud-145-109-20-58.wireless.uva.nl>
2016-11-03 16:49:27 +0100

    nodes.py aangemaakt

---
+++ nodes.py
@@ -0,0 +1,9 @@
+class Gates(object):
+	""" nodes/gates class """
+
+	def __init__(self, coordinates, id):
+		self.coordinates = coordinates
+		self.gate_id = gate_id
+
+	def getCoordinates(self, coordinates):
+		return coordinates
