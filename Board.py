566b2b24f7a2166afaafececa99232c05e78669a
Harm <harm@bit-students.com>
2016-11-10 10:27:14 +0100

    Started visualisation

--- Board.py
+++ Board.py
@@ -1,14 +1,23 @@
-
+import pprint
 
 class Board(object):
 	"""docstring for Board"""
 	def __init__(self, x_dim, y_dim):
-		super(Board, self).__init__()
-		self.dimensions = (x_dim, y_dim)
-		self.board = [][][]
+		# # super(Board, self).__init__()
+		# self.dimensions = (x_dim, y_dim)
+		self.board = [[[None for k in range(1)] for j in range(y_dim)] for i in range(x_dim)]
 
 	def getElementAt(self, x, y, z):
 		return self.board[x][y][z]
 
 	def setElementAt(self, x, y, z, object):
-		if getElementAt(x, y, z) == None:
+		pass
+
+	def printBoard(self):
+		pprint.pprint(self.board)
+
+
+if __name__ == '__main__':
+	b = Board(3,3)
+
+	pprint.pprint(b.board)
