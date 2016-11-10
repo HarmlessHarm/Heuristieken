6c659cb25df5656add06d8abc1924afac6231759
Harm <harm@bit-students.com>
2016-11-10 10:53:10 +0100

    Used NP 3d array to represent board

--- Board.py
+++ Board.py
@@ -1,19 +1,14 @@
-import pprint
+import pprint, numpy as np
 
-# class Board(object):
-# 	"""docstring for Board"""
-# 	def __init__(self, x_dim, y_dim):
-# 		# # super(Board, self).__init__()
-# 		# self.dimensions = (x_dim, y_dim)
-# 		self.board = [[[None for k in range(1)] for j in range(y_dim)] for i in range(x_dim)]
-# =======
 class Board(object):
 	"""docstring for Board"""
-	def __init__(self, x_dim, y_dim):
+	def __init__(self, x_dim, y_dim, z_dim=1):
 		super(Board, self).__init__()
-		z_dim = 1
-		self.dimensions = (x_dim, y_dim, z_dim)
-		self.board = [[[0]*z_dim]*y_dim]*x_dim
+		self.x_dim = x_dim
+		self.y_dim = y_dim
+		self.z_dim = z_dim
+
+		self.board = np.zeros((y_dim,x_dim,z_dim))
 
 	def getElementAt(self, x, y, z):
 		return self.board[x][y][z]
@@ -23,14 +18,14 @@ class Board(object):
 			self.board[x][y][z] = gateID
 
 	def prettyPrintLayer(self, layer):
-		for y in range(0, self.dimensions[1]):
-			print(self.board[:][y])
+		print b.board[:,:,layer]
 
 	def printBoard(self):
-		pprint.pprint(self.board)
+		for z in range(self.z_dim):
+			self.prettyPrintLayer(z)
+			print "\n"
 
 
 if __name__ == '__main__':
-	b = Board(3,3)
-
-	pprint.pprint(b.board)
+	b = Board(3,4,3)
+	b.printBoard()
