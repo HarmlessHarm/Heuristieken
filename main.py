d446020279252eaa2c94c24b7f52fd963271d5fb
Harm <harm@bit-students.com>
2016-11-10 14:17:58 +0100

    created small test board and test netlist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Board import Board
-from Visualizer import Visualizer
+from Objects import Board, Gate, Net
+# from Visualizer import Visualizer
 import json
 
 
@@ -8,13 +8,13 @@ file = 'prints.json'
 with open(file) as json_data:
 
     d = json.load(json_data)
-    board = d['prints'][0]
+    board = d['prints'][2]
     width = board['width']
     height = board['height']
-    b = Board.Board(width, height)
+    b = Board(width, height)
     for gate in board['gates']:
-        b.setElementAt(gate['x'], gate['y'], 0, gate['id'])
+    	g = Gate(gate['x'], gate['y'], gate['id'])
+        b.setElementAt(g.gate_id, g.x, g.y)
         # print('x: '+ str(gate['x']) + ', y: ' + str(gate['y']) + ', id: ' + str(gate['id']))
         #b.setElementAt(gate['x'], gate['y'], 0, gate['id'])
     print b.getLayer(0)
-
