f10af79fed192d8f54a4274e7668d47aec4591d8
Harm <harm@bit-students.com>
2016-11-10 14:17:37 +0100

    Changed file struct

--- Net.py
+++
@@ -1,8 +0,0 @@
-class Net:
-
-    def __init__(self, id, start, end):
-        super(Net, self).__init__()
-        self.identifier = identifier
-        self.start = start
-        self.end = end
-        self.path = []
