775fdc3c5131913f99653610211ce6f324b2ae74
Aynel Gül <Aynel@wcw-stud-145-109-52-110.wireless.uva.nl>
2016-11-10 16:24:59 +0100

    nestlist dictionary

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from __future__ import print_function
 from Board import Board
-from Visualizer import Visualizer
+# from Visualizer import Visualizer
 import json
+
+netlist_file = 'netlist.txt'
+
+with open(netlist_file, 'r') as netlist_data:
+    nd = netlist_data.readlines()
+    net_dict = {}
+    for line in nd:
+        if line[0] == 'n':
+            line = line.split(' = ')
+            # print(line, end='')
+            net_dict[line[0]] = line[1]
+        else:
+            continue
+    print(net_dict)
 
 
 file = 'prints.json'
@@ -8,14 +23,15 @@ file = 'prints.json'
 with open(file) as json_data:
 
     d = json.load(json_data)
-    board = d['prints'][0]
-    width = board['width']
-    height = board['height']
-    b = Board(width, height)
-    for gate in board['gates']:
-        b.setElementAt(gate['x'], gate['y'], 0, gate['id'])
-        # print('x: '+ str(gate['x']) + ', y: ' + str(gate['y']) + ', id: ' + str(gate['id']))
-        #b.setElementAt(gate['x'], gate['y'], 0, gate['id'])
-    v = Visualizer(b)
-    v.start()
+
+board = d['prints'][0]
+width = board['width']
+height = board['height']
+b = Board(width, height)
+for gate in board['gates']:
+    b.setElementAt(gate['x'], gate['y'], 0, gate['id'])
+    # print('x: '+ str(gate['x']) + ', y: ' + str(gate['y']) + ', id: ' + str(gate['id']))
+    #b.setElementAt(gate['x'], gate['y'], 0, gate['id'])
+# v = Visualizer(b)
+# v.start()
 
