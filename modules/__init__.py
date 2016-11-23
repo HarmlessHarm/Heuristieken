c70a7f8cbd602bb066288fe4b1dbebb6ee90482f
Harm <harm@bit-students.com>
2016-11-23 15:34:06 +0100

    removed comments

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,18 +1,5 @@
 from Objects import *
 from Algorithms import *
 from Visualizer import *
-# import pkgutil, inspect
 
 __all__ = ["Board", "Net", "Gate", "EasyPath", "AStar", "Dijkstra", "runAlgorithm", "Visualizer"]
-
-# for loader, name, is_pkg in pkgutil.walk_packages(__path__):
-# 	module = loader.find_module(name).load_module(name)
-
-# 	for name, value in inspect.getmembers(module):
-# 		if name.startswith('__'):
-# 			continue
-
-# 		globals()[name] = value
-# 		__all__.append(name)
-
-# print __all__
