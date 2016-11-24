7657fda3fa3eb7f04c7fe85491b963702e07a0ef
Harm <harm@bit-students.com>
2016-11-24 16:49:06 +0100

    Added astar function to experiements

--- experiments.py
+++ experiments.py
@@ -1,23 +1,38 @@
 from modules import *
+import matplotlib.pyplot as plt
 
-def randomSamples(iterations):
+def randomSamples(iterations, bi, ni):
 	netlists = readNetlists()
-	n = netlists[0]
+	n = netlists[ni]
 	l = []
 	validLists = []
+	filename = 'validLists-b'+str(bi+1)+'-n'+str(ni+1)+'.txt'
 	for i in range(iterations):
 		print i,
-		b = createBoard(0, 30)
+		b = createBoard(bi, 30)
 		s = Sorter(n,b)
 		s.random()
-		if runAlgorithm('astar', s.board, s.netlist, True):
+		if runAlgorithm('astar', s.board, s.netlist):
 			validLists.append(s.netlist)
 			solved, score = s.board.getScore()
+
+			with open(filename, 'a') as wf:
+				wf.write(str(score)+ " : " + str(s.netlist) + "\n")
 			l.append(score)
 	# a = np.histogram(l)
 	# print a
-	print validLists
-	plt.hist(l)
-	plt.show()
+	# print validLists
+	# plt.hist(l)
+	# plt.show()
 
-randomSamples(5)
+def astar(netlist, alg_str):
+	board = createBoard(0,30)
+	if runAlgorithm(alg_str, board, netlist):
+		print board.getScore()
+		v = Visualizer(board)
+		v.start()
+
+
+netlist = [(19, 2), (2, 20), (3, 15), (1, 0), (15, 21), (3, 23), (20, 10), (16, 9), (5, 7), (15, 8), (11, 24), (23, 8), (3, 0), (7, 13), (10, 4), (22, 16), (9, 13), (10, 7), (13, 18), (3, 4), (22, 13), (3, 5), (7, 9), (23, 4), (19, 5), (15, 5), (6, 14), (22, 11), (15, 17), (20, 19)]
+randomSamples(500, 0, 0)
+astar(netlist, 'astar')
--- main.py
+++ main.py
@@ -1,40 +1,9 @@
 from modules import *
-# import numpy as np
-# import json, ast, 
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
 
-# def readNetlists():
 
-# 	netlist_file = 'resources/netlist.txt'
-# 	net_lists = []
-# 	with open(netlist_file, 'r') as netlist_data:
-# 	    nd = netlist_data.readlines()
-# 	    for line in nd:
-# 	        if line[0] == 'n':
-# 	            line = line.split(' = ')
-# 	            net_lists.append(ast.literal_eval(line[1]))
-# 	        else:
-# 	            continue
-
-# 	return net_lists
-		
-# def createBoard(i, layers):
-
-# 	file = 'resources/prints.json'
-
-# 	with open(file) as json_data:
-
-# 	    d = json.load(json_data)
-# 	    board = d['prints'][i]
-# 	    width = board['width']
-# 	    height = board['height']
-# 	    b = Board(width, height, layers)
-# 	    for gate in board['gates']:
-# 	    	g = Gate(gate['id']-1, gate['x'], gate['y'])
-# 	    	b.gates[g.gate_id] = (g.x, g.y, g.z)
-# 	        b.setElementAt(g, g.x, g.y)
-
-# 	    return b
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
