a073386ae727d500d5832c1e29f71ca0344fa8d8
Harm Manders <harmmanders@gmail.com>
2016-12-09 16:28:38 +0100

    Finished documentation for Board object

--- modules/Objects.py
+++ modules/Objects.py
@@ -2,13 +2,15 @@
 import pprint
 import copy
 import numpy as np
+from future import __division__
 
 class Board(object):
+
     """
     Board object in which all data is stored
     """
 
-    def __init__(self, x_dim, y_dim, z_dim=1):
+    def __init__(self, x_dim, y_dim, z_dim=10):
         """
         Creates a Board object with certain dimensions and initializes a 3d 
         numpy array which represents the circuit.
@@ -18,7 +20,7 @@ class Board(object):
         Args:
             x_dim (int): Width of the circuit
             y_dim (int): Height of the circuit
-            z_dim (int): Maximum depth of the circuit
+            z_dim (int, optional): Maximum depth of the circuit. Defaults to 10
         """
         super(Board, self).__init__()
         self.x_dim = x_dim
@@ -30,16 +32,29 @@ class Board(object):
 
     def getElementAt(self, x, y, z):
         """
-        Returns the object that is positioned at a certain position on the board
+        Returns the object that is positioned at a certain position on the board.
 
         Args:
-            x (int):
+            x,y,z (int): x,y,z coordinate.
+        Returns:
+            Object at certain coordinate.
+
+            Returns False if one of the provided coordinates are out of bound 
+            of the board.
         """
-        if  0 <= x < self.x_dim and 0 <= y < self.y_dim and 0 <= z < self.z_dim:
+        if 0 <= x < self.x_dim and 0 <= y < self.y_dim and 0 <= z < self.z_dim:
             return self.board[x, y, z]
         return False
 
     def isEmpty(self, xyz):
+        """ 
+        Checks if board at a certain position is empty.
+
+        Args:
+            xyz (int tuple): Contains the x, y and z coordinates.
+        Returns:
+            bool: True if cell is empty, False otherwise.
+        """
         x, y, z = xyz
         if self.getElementAt(x, y, z) is 0:
             return True
@@ -47,43 +62,78 @@ class Board(object):
             return False
 
     def setElementAt(self, obj,  x, y, z=0):
-        if self.isEmpty((x,y,z)):
+        """
+        Puts an object in a cell at a certain coordinate if the cell is empty.
+    
+        This function can be used to set Gate and Net objects.
+
+        Args:
+            obj (object): Object that is set at position.
+            x,y,z (int): x,y,z coordinate. Z Defaults at 0
+        Returns:
+            bool: True if element is set. False if target 
+        """
+        if self.isEmpty((x, y, z)):
             self.board[x, y, z] = obj
             return True
         else:
             return False
 
     def removeElementAt(self, xyz):
+        """
+        Deletes object from cell at certain coordinate.
+
+        Args:
+            xyz (int tuple): Contains the x, y and z coordinates.
+        Returns:
+            bool: True when finished.    
+        """
         self.board[xyz] = 0
         return True
 
-    def addLayer(self):
-        x, y = (self.x_dim, self.y_dim)
-        layer = np.zeros((x,y,1))
-        self.z_dim += 1
-        self.board = np.append(self.board, layer, axis=2)
-
-    def getLayer(self, layer):
-        return self.board[:, :, layer]
-
     def getDimensions(self):
+        """
+        Returns x, y and z dimensions of the board.
+        """
         return (self.x_dim, self.y_dim, self.z_dim)
 
     def getAllNeighbours(self, x, y, z):
-        xyz = (x,y,z)
+        """
+        Returns a list with all neighbours of a cell at certain coordinate.
+
+        Uses a difference vector to get neighbours in x,y,z directions.
+        Checks if neighbour is in bound of the board.
+
+        Args:
+            x,y,z (int): x,y,z coordinates
+        Returns: 
+            neighbours (tuple list): List of all coordinates of the neighbours 
+                of the cell.
+        """
+        xyz = (x, y, z)
         neighbours = []
-        xs = [-1,1,0,0,0,0]
-        ys = [0,0,-1,1,0,0]
-        zs = [0,0,0,0,-1,1]
-        
-        for dif_tuple in zip(xs,ys,zs):
-            (nx,ny,nz) = [sum(x) for x in zip(xyz, dif_tuple)]
-            if 0 <= nx < self.x_dim and 0 <= ny < self.y_dim and 0 <= nz < self.z_dim and xyz != (nx,ny,nz):
-                neighbours.append((nx,ny,nz))
+        xs = [-1, 1, 0, 0, 0, 0]
+        ys = [0, 0, -1, 1, 0, 0]
+        zs = [0, 0, 0, 0, -1, 1]
+
+        for dif_tuple in zip(xs, ys, zs):
+            (nx, ny, nz) = [sum(x) for x in zip(xyz, dif_tuple)]
+            if 0 <= nx < self.x_dim and 0 <= ny < self.y_dim and 
+                0 <= nz < self.z_dim and xyz != (nx, ny, nz):
+                neighbours.append((nx, ny, nz))
         return neighbours
-        
+
     def getOpenNeighbours(self, x, y, z):
-        neighbours = self.getAllNeighbours(x,y,z)
+        """
+        Returns a list of all empty neighbours of a cell at a certain coordinate.
+
+        Args:
+            x,y,z (int): x,y,z coordinates
+        Returns:
+            openNeighbours (tuple list): List of all coordinates of the empty 
+                neighbours of the cell.
+        """
+        neighbours = self.getAllNeighbours(x, y, z)
         openNeighbours = []
         for n in neighbours:
             if self.isEmpty(n):
@@ -91,6 +141,18 @@ class Board(object):
         return openNeighbours
 
     def getScore(self):
+        """
+        Returns the number of paths and the score of the board.
+
+        Calculates the score by summing the length of each path of a net 
+        on the board.
+        The number of paths represents the amount of completed nets. Optimally
+        this should be the same as the length of the netlist.
+
+        Returns:
+            pathCount (int): Number of paths
+            score (int): Score of a board
+        """
         score = 0
         pathCount = 0
         for i in self.nets:
@@ -101,6 +163,16 @@ class Board(object):
         return pathCount, score
 
     def getMinimumScore(self):
+        """
+        Returns the minimum score a board can have.
+
+        Calculates the score by using the manhattan distance between the start
+        and end points of each net in the board. This score is used as a 
+        comparison for the real score.
+
+        Returns:
+            score (int): 
+        """
         score = 0
         for netID, netObject in self.nets:
             # returns all net objects
@@ -119,43 +191,49 @@ class Board(object):
         return score
 
     def getRelativeScore(self):
+        """
+        Returns the relative score between minimum and actual score.
+        """
         minimum = getMinimumScore()
         current = getScore()
         # check how many times current score fits into min. score
-        return current[1] / (minimum + 0.0)     
-        
-    def printBoard(self):
-        for z in range(self.z_dim):
-            print 'layer',z
-            print self.getLayer(z)
-            print "\n"
+        return current[1] / minimum
 
     def setNetPath(self, net):
-        for (x,y,z) in net.path[1:-1]:
-            if not self.setElementAt(net, x,y,z):
+        """
+        Iterates through the path in a net and sets the corresponding cells.
+
+        Sets Net objects at the corresponding cells.
+
+        Args:
+            net (Net): net that is iterated.
+        Returns:
+            bool: True if completed. False if a position isn't valid.
+        """
+        for (x, y, z) in net.path[1:-1]:
+            if not self.setElementAt(net, x, y, z):
                 return False
         return True
 
     def removeNetPath(self, net):
+        """
+        Removes all objects from the board at the corresponding path.
+
+        Iterates over the path and removes objects from the board. 
+        Empties the path attribute of the Net opbject.
+
+        Args:
+            net (Net): net that is used.
+        Returns:
+            bool: True if completed. False if not removed.
+        """
         if net.path != []:
-            for (x,y,z) in net.path[1:-1]:
-                self.removeElementAt((x,y,z))
+            for (x, y, z) in net.path[1:-1]:
+                self.removeElementAt((x, y, z))
             net.path = []
             return True
-        else: 
+        else:
             return False
-
-
-    def copy(self):
-        newBoard = type(self)(self.x_dim, self.y_dim, self.z_dim)
-        newArray = np.deepcopy(self.board)
-        newGates = copy.deepcopy(self.gates)
-        newNets = copy.deepcopy(self.nets)
-        newBoard.board = newArray
-        newBoard.gates = newGates
-        newBoard.nets = newNets
-        return newBoard
-
 
 class Gate(object):
 
@@ -171,7 +249,9 @@ class Gate(object):
     def getCoordinates(self, coordinates):
         return (x, y, z)
 
+
 class Net(object):
+
     """ Docstring for Net """
 
     def __init__(self, start_gate, end_gate, net_id):
@@ -181,12 +261,15 @@ class Net(object):
         self.end_gate = end_gate
         self.path = []
 
-    def addPos(self,xyz):
-        x, y ,z = xyz
-        self.path.append((x,y,z))
+    def addPos(self, xyz):
+        x, y, z = xyz
+        self.path.append((x, y, z))
+
 
 class TreeNode(object):
+
     """docstring for TreeNode"""
+
     def __init__(self, board, previousNode, netlist):
         super(TreeNode, self).__init__()
         self.board = board
