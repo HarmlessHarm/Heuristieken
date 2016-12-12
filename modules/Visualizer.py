ea79ec5d58345957c1eb78977de15a96e703e78f
Aynel <aynell__@hotmail.com>
2016-12-12 12:25:45 +0100

    visualizer met eventueel twee boards, inc scores

--- modules/Visualizer.py
+++ modules/Visualizer.py
@@ -6,28 +6,50 @@ import numpy as np
 
 class Visualizer(object):
 
-	def __init__(self, board):
+	def __init__(self, board, board2 = False):
 		self.board = board
+		self.board2 = board2
 
 	def start(self):
-		fig = plt.figure()
-		ax = fig.add_subplot(111,projection='3d')
-		ax.set_zlim([0,self.board.z_dim])
-		
+		fig = plt.figure(figsize=(15, 8))
+		# title centered above all subplots
+		st = fig.suptitle("Boards", fontsize = "x-large")
 
-		# self.plotGridLines(ax)
-		self.plotGates(ax)
-		self.plotNets(ax)
-		ax.set_xlabel('width')
-		ax.set_ylabel('height')
-		ax.set_zlabel('layer')
+		if self.board2 is False:
+			ax1 = fig.add_subplot(111, projection = '3d')
+		else:
+			ax1 = fig.add_subplot(121, projection = '3d')
+
+		ax1.set_zlim([0,self.board.z_dim])
+		ax1.set_title("Absolute score: " + str(self.board.getScore()[1]) + "\n Relative score: " + str(self.board.getRelativeScore()))
+
+		# self.plotGridLines(ax1)
+		self.plotGates(ax1, self.board)
+		self.plotNets(ax1, self.board)
+		ax1.set_xlabel('width')
+		ax1.set_ylabel('height')
+		ax1.set_zlabel('layer')
 		# plt.axis('off')
+
+		if self.board2 is not False:
+			ax2 = fig.add_subplot(122, projection = '3d')
+			ax2.set_zlim([0,self.board2.z_dim])
+			ax2.set_title("Absolute score: " + str(self.board2.getScore()[1]) + "\n Relative score: " + str(self.board2.getRelativeScore()))
+
+			# self.plotGridLines(ax2)
+			self.plotGates(ax2, self.board2)
+			self.plotNets(ax2, self.board2)
+			ax2.set_xlabel('width')
+			ax2.set_ylabel('height')
+			ax2.set_zlabel('layer')
+			# plt.axis('off')
+
 		plt.show()
 
-	def plotGates(self, ax):
+	def plotGates(self, ax, board):
 		#print self.board.gates
-		for g in self.board.gates:
-			x,y,z = self.board.gates[g].getCoordinates()
+		for g in board.gates:
+			x,y,z = board.gates[g].getCoordinates()
 			ax.scatter(x,y,z, c='r', marker='s')
 			ax.text(x,y,z,str(g),color='k', fontsize=12)
 
@@ -38,8 +60,8 @@ class Visualizer(object):
 		for j in range(height+1):
 			ax.plot([width, 0], [j,j], c='0.2')
 	
-	def plotNets(self, ax):
-		nets = self.board.nets
+	def plotNets(self, ax, board):
+		nets = board.nets
 		cmap = plt.get_cmap('gnuplot')
 		colors = [cmap(i) for i in np.linspace(0, 1, len(nets))]
 		for net_id, color in zip(nets.keys(),colors):
